ab02eaef91d10277c4604bc8fd643b9fae4c03dd
Matt Steele <orphum@gmail.com>
2017-08-13 13:53:20 -0500

    Update Webpack/phaser, make game actually work

--- src/game.js
+++ src/game.js
@@ -2,18 +2,20 @@ import makeGame from './flappy';
 import SpeedCadence from './bt/cycling';
 const bike = new SpeedCadence();
 
-const [game, mainState] = makeGame();
 document.addEventListener('DOMContentLoaded', () => {
+  const [game, mainState] = makeGame(document.querySelector('.game'));
+  // Start the state to actually start the game
+  game.state.start('main');
   document.querySelector('button').addEventListener('click', async _ => {
     await bike.connect();
+
+    // Listen to cadence stream
     bike.parsedCadence$().addListener({
       next: (val) => { 
         mainState.moveBird(val);
       },
       error(err) { console.error(err) }
     });
-    // Start the state to actually start the game
-    game.state.start('main');
   });
 });
 
