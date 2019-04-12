242b45bcee1eec69a5962841150893a6afec8361
Matt Steele <orphum@gmail.com>
2019-04-11 22:43:25 -0500

    Make static assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 /* global __dirname */
 
-const path = require('path');
+const path = require("path");
 
-const webpack = require('webpack');
-const BundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require("webpack");
+const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-const dirSrc = path.resolve(__dirname, 'src');
-const dirTest = path.resolve(__dirname, 'test');
-const public = path.resolve(__dirname, 'public');
-const dirBuild = path.resolve(__dirname, 'web-bluetooth');
-const dirDist = path.resolve(__dirname, 'dist');
-const dirDev = path.resolve(__dirname, 'dev');
+const dirSrc = path.resolve(__dirname, "src");
+const dirTest = path.resolve(__dirname, "test");
+const public = path.resolve(__dirname, "public");
+const dirBuild = path.resolve(__dirname, "flappy");
+const dirDist = path.resolve(__dirname, "dist");
+const dirDev = path.resolve(__dirname, "dev");
 
 const args = process.argv.slice(2);
 
@@ -20,22 +20,22 @@ let devtool;
 let plugins;
 
 entry = {
-  slides: './src/index.js',
-  game: './src/game.js'
+  slides: "./src/index.js",
+  game: "./src/game.js"
 };
 output = {
   path: dirBuild,
-  filename: '[name].js'
+  filename: "[name].js"
 };
 plugins = [
   // Avoid publishing files when compilation fails
   new webpack.NoEmitOnErrorsPlugin()
 ];
 // Create Sourcemaps for the bundle
-devtool = 'source-map';
+devtool = "source-map";
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry,
   output,
   plugins,
